331b43deaba5eb10eb31e273fab6a942da40793d
Zach Mays <zsmays@gmail.com>
2020-05-04 10:56:48 -0500

    bash to sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ echo "Attempting to update kubeconfig for aws"
 
 aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
 aws sts get-caller-identity
-bash "$@"
+sh "$@"
